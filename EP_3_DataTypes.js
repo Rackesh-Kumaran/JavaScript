75756319e199caccc1ac03116c64d22ebbd851f9
Rackesh-Kumaran <rackeshkumaran4@gmail.com>
2025-01-31 19:57:09 +0530

    Add files via upload

---
+++ EP_3_DataTypes.js
@@ -0,0 +1,45 @@
+// string
+
+let country = "USA";
+console.log(country)  //output = USA
+
+
+// Number
+
+let n1 = 234;
+let f1 = 23.4;
+console.log(n1); // op = 234
+console.log(f1); // op = 23.4
+
+
+
+// Boolean 
+
+let datachecked = true;
+let dataNotChecked = false;
+console.log(datachecked); // op = true
+console.log(dataNotChecked); // op = false
+
+
+
+// undefined 
+
+let a ;
+console.log(a) // op = undefined
+
+
+// Null
+
+let b = null;
+console.log(b); // op = null
+
+
+// object 
+
+let student = {
+  firstName: "John",
+  lastName: null,
+  class: 10,
+};
+
+console.log(student); 
